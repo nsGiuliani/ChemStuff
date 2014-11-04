0d659db6b9fd1e12175456c72c56adfbfd0eb789
nsGiuliani <nick@Ein.(none)>
2014-11-04 14:59:14 -0600

    broke program up into func

--- tenAtomAdd.cpp
+++ tenAtomAdd.cpp
@@ -27,7 +27,7 @@ void simulation(int numAtoms, double temperature) {
 	double Fx;
 	double Fy;
 	double Fz;
-	int iterations = 50000;
+	int iterations = 5000;
 	double timeStep = .00000000000001;
 	double ang = 0.0000000001;
 	double ePot = 0;
@@ -108,7 +108,9 @@ void simulation(int numAtoms, double temperature) {
 			}
 
 			atoms[k].set_Coor(x,y,z);
+		}
 			//find the minimum distance to other atoms and update vel
+		for (int k=0; k<numAtoms; k++) { //k is the atom which is being updated
 			for (int l = 0; l<numAtoms;l++){ //l is the atom whose presence is applying force to k
 				double min = 1000000000;
 				if(l != k){ //make sure they are not the same atom
@@ -205,6 +207,218 @@ void simulation(int numAtoms, double temperature) {
 		}
 	}
 }
+void makeBox(double box) {
+	ofstream fout;
+	fout.open("box.xyz");
+	fout << 12*box << endl;
+	fout << "box " << endl;
+	for (int b =0; b<box; b++) {
+		
+		fout<<"box   "<< b <<"	"<< 0 << "	"<< 0 << endl;
+		fout<<"box   "<< 0 <<"	"<< b << "	"<< 0 << endl;
+		fout<<"box   "<< 0 <<"	"<< 0 << "	"<< b << endl;
+		fout<<"box   "<< box <<"	"<< b << "	"<< 0 << endl;
+		fout<<"box   "<< box <<"	"<< 0 << "	"<< b << endl;
+		fout<<"box   "<< b <<"	"<< box << "	"<< 0 << endl;
+		fout<<"box   "<< 0 <<"	"<< box << "	"<< b << endl;
+		fout<<"box   "<< b <<"	"<< 0 << "	"<< box << endl;
+		fout<<"box   "<< 0 <<"	"<< b << "	"<< box << endl;
+		fout<<"box   "<< box <<"	"<< box << "	"<< b << endl;
+		fout<<"box   "<< b <<"	"<< box << "	"<< box << endl;
+		fout<<"box   "<< box <<"	"<< b << "	"<< box << endl;
+	}
+	fout.close();
+}
+
+void updateCoor(vector<Atom>& atoms, int numAtoms, int i, double box, double timeStep) {
+	double x;
+	double y;
+	double z;
+	double ang = 0.0000000001;
+		//ofstream fout;
+		ofstream fout;
+		fout.open("10Atom.xyz", ios::app);
+		//print to .xyz file
+		//if(i==0){
+		fout << numAtoms << endl;
+		//}
+		fout << "iteration " << i << endl;
+		for (int k=0; k<numAtoms; k++) {
+			fout<< "Argon	";
+			fout<< atoms[k].getxCoor() <<"	"<<atoms[k].getyCoor() << "	"<< atoms[k].getzCoor()<< endl;
+		}
+		for (int k=0; k<numAtoms; k++) { //k is the atom which is being updated
+			//update the coordinates and make sure they are in the box
+			x= atoms[k].getxCoor() + atoms[k].getxVel()*timeStep/ang;
+			y= atoms[k].getyCoor() + atoms[k].getyVel()*timeStep/ang;
+			z= atoms[k].getzCoor() + atoms[k].getzVel()*timeStep/ang;
+			if(x >= box){
+				x =fmod(x, box);
+			}
+			if(x <0) {
+				x =fmod(x, box) + box;
+			}
+			if(y >= box){
+				y = fmod(y, box);
+			}
+			if(y <0) {
+				y =fmod(y, box) +box;
+			}
+			if(z >= box){
+				z =fmod(z, box);
+			}
+			if(z <0) {
+				z =fmod(z, box) + box;
+			}
+
+			atoms[k].set_Coor(x,y,z);
+		}
+}
+
+void updateVel(vector<Atom>& atoms, int numAtoms, double box, double timeStep) {
+	double dist;
+	double dx=0.0;
+	double dy=0.0;
+	double dz=0.0;
+
+	double ang = 0.0000000001;
+	double E = 1.712*(.000000000000000000001);
+	double sig = 3.4*(.0000000001);
+	double Fx;
+	double Fy;
+	double Fz;
+	double x;
+	double y;
+	double z;
+	for (int k=0; k<numAtoms; k++) { //k is the atom which is being updated
+			for (int l = 0; l<numAtoms;l++){ //l is the atom whose presence is applying force to k
+				double min = 1000000000;
+				if(l != k){ //make sure they are not the same atom
+					dist = (atoms[l].getxCoor()-atoms[k].getxCoor())*(atoms[l].getxCoor()-atoms[k].getxCoor())
+						+(atoms[l].getyCoor()-atoms[k].getyCoor())*(atoms[l].getyCoor()-atoms[k].getyCoor())
+						+ (atoms[l].getzCoor()-atoms[k].getzCoor())*(atoms[l].getzCoor()-atoms[k].getzCoor());
+					if (dist < min) {
+						min = dist;
+						dx =atoms[l].getxCoor()-atoms[k].getxCoor();
+						dy =atoms[l].getyCoor()-atoms[k].getyCoor();
+						dz =atoms[l].getzCoor()-atoms[k].getzCoor();
+					}
+					dist = (atoms[l].getxCoor()-atoms[k].getxCoor()-box)*(atoms[l].getxCoor()-atoms[k].getxCoor()-box)
+						+(atoms[l].getyCoor()-atoms[k].getyCoor())*(atoms[l].getyCoor()-atoms[k].getyCoor())
+						+ (atoms[l].getzCoor()-atoms[k].getzCoor())*(atoms[l].getzCoor()-atoms[k].getzCoor());					
+					if (dist < min) {
+						min = dist;
+						dx =atoms[l].getxCoor()-atoms[k].getxCoor()-box;
+						dy =atoms[l].getyCoor()-atoms[k].getyCoor();
+						dz =atoms[l].getzCoor()-atoms[k].getzCoor();
+					}			
+					dist = (atoms[l].getxCoor()-atoms[k].getxCoor()+box)*(atoms[l].getxCoor()-atoms[k].getxCoor()+box)
+						+(atoms[l].getyCoor()-atoms[k].getyCoor())*(atoms[l].getyCoor()-atoms[k].getyCoor())
+						+ (atoms[l].getzCoor()-atoms[k].getzCoor())*(atoms[l].getzCoor()-atoms[k].getzCoor());					
+					if (dist < min){
+						min = dist;
+						dx =atoms[l].getxCoor()-atoms[k].getxCoor()+box;
+						dy =atoms[l].getyCoor()-atoms[k].getyCoor();
+						dz =atoms[l].getzCoor()-atoms[k].getzCoor();
+					}			
+					dist = (atoms[l].getxCoor()-atoms[k].getxCoor())*(atoms[l].getxCoor()-atoms[k].getxCoor())
+						+(atoms[l].getyCoor()-atoms[k].getyCoor()-box)*(atoms[l].getyCoor()-atoms[k].getyCoor()-box)
+						+ (atoms[l].getzCoor()-atoms[k].getzCoor())*(atoms[l].getzCoor()-atoms[k].getzCoor());					
+					if (dist < min){
+						min = dist;
+						dx =atoms[l].getxCoor()-atoms[k].getxCoor();
+						dy =atoms[l].getyCoor()-atoms[k].getyCoor()-box;
+						dz =atoms[l].getzCoor()-atoms[k].getzCoor();
+					}
+					dist = (atoms[l].getxCoor()-atoms[k].getxCoor())*(atoms[l].getxCoor()-atoms[k].getxCoor())
+						+(atoms[l].getyCoor()-atoms[k].getyCoor()+box)*(atoms[l].getyCoor()-atoms[k].getyCoor()+box)
+						+ (atoms[l].getzCoor()-atoms[k].getzCoor())*(atoms[l].getzCoor()-atoms[k].getzCoor());					
+					if (dist < min){
+						min = dist;
+						dx =atoms[l].getxCoor()-atoms[k].getxCoor();
+						dy =atoms[l].getyCoor()-atoms[k].getyCoor()+box;
+						dz =atoms[l].getzCoor()-atoms[k].getzCoor();
+					}		
+					dist = (atoms[l].getxCoor()-atoms[k].getxCoor())*(atoms[l].getxCoor()-atoms[k].getxCoor())
+						+(atoms[l].getyCoor()-atoms[k].getyCoor())*(atoms[l].getyCoor()-atoms[k].getyCoor())
+						+ (atoms[l].getzCoor()-atoms[k].getzCoor()-box)*(atoms[l].getzCoor()-atoms[k].getzCoor()-box);					
+					if (dist < min){
+						min = dist;
+						dx =atoms[l].getxCoor()-atoms[k].getxCoor();
+						dy =atoms[l].getyCoor()-atoms[k].getyCoor();
+						dz =atoms[l].getzCoor()-atoms[k].getzCoor()-box;
+					}
+					dist = (atoms[l].getxCoor()-atoms[k].getxCoor())*(atoms[l].getxCoor()-atoms[k].getxCoor())
+						+(atoms[l].getyCoor()-atoms[k].getyCoor())*(atoms[l].getyCoor()-atoms[k].getyCoor())
+						+ (atoms[l].getzCoor()-atoms[k].getzCoor()+box)*(atoms[l].getzCoor()-atoms[k].getzCoor()+box);					
+					if (dist < min){
+						min = dist;
+						dx =atoms[l].getxCoor()-atoms[k].getxCoor();
+						dy =atoms[l].getyCoor()-atoms[k].getyCoor();
+						dz =atoms[l].getzCoor()-atoms[k].getzCoor()+box;
+					}
+					dx= dx*ang;
+					dy=dy*ang;
+					dz=dz*ang;
+					// update the velocity 
+					double D = dx*dx+dy*dy+dz*dz;
+					double D_7= (dx*dx+dy*dy+dz*dz)*(dx*dx+dy*dy+dz*dz)*(dx*dx+dy*dy+dz*dz)*(dx*dx+dy*dy+dz*dz)*(dx*dx+dy*dy+dz*dz)*(dx*dx+dy*dy+dz*dz)
+						*(dx*dx+dy*dy+dz*dz);
+					double D_4 = (dx*dx+dy*dy+dz*dz)*(dx*dx+dy*dy+dz*dz)*(dx*dx+dy*dy+dz*dz)*(dx*dx+dy*dy+dz*dz);
+					double sig_6= sig*sig*sig*sig*sig*sig;
+					Fx = -1*(24*E*sig_6*dx*((2*sig_6/(D_7))-(1/(D_4))));
+					Fy = -1*(24*E*sig_6*dy*((2*sig_6/(D_7))-(1/(D_4))));					
+					Fz = -1*(24*E*sig_6*dz*((2*sig_6/(D_7))-(1/(D_4))));
+					x= atoms[k].getxVel() + (Fx/atoms[k].getMass())*timeStep;
+					y= atoms[k].getyVel() + (Fy/atoms[k].getMass())*timeStep;
+					z= atoms[k].getzVel() + (Fz/atoms[k].getMass())*timeStep;
+					atoms[k].set_Vel(x,y,z);
+				}		
+																	
+			}
+		}		
+}
+
+void cleanFile() {
+	ofstream fout;
+	fout.open("10Atom.xyz");
+	fout<< "";
+}
+
+double defBox(int numAtoms,double temperature) {
+	double box;
+	double ang = 0.0000000001;
+	//calculate the volume of the box using PV = nRT (P = 1)
+	double n = numAtoms/(6.022*pow(10, 23)); //calculate the mols of atoms
+	double R = .08206; //gas constant
+	box = n*R*temperature; //calculate the volume of the box in liters
+	box *= .001; //convert the volume of the box from liters to meters cubed
+	box = pow(box, 1/3.); //take the cube root to find the length of one side of the box
+	box /= ang; //convert to angstroms
+	return box;
+}
+
+void simulation2(int numAtoms, double temperature){
+
+	vector<Atom> atoms;
+
+	int iterations = 5000;
+	double timeStep = .00000000000001;
+
+	double box = defBox(numAtoms, temperature);
+	makeBox(box);
+	cleanFile();
+
+	//set initial positions and velocities for atoms
+	for(double i =0; i<numAtoms; i++) {
+		atoms.push_back(Atom(39.948*1.66053892 * pow(10,-27),fmod(rand(),box),fmod(rand(),box),fmod(rand(),box),fmod(rand(),500)-250,fmod(rand(),500)-250,fmod(rand(),500)-250));
+	}
+	for(int i = 0; i<iterations; i++) {
+		updateCoor(atoms, numAtoms, i, box, timeStep);
+		updateVel(atoms, numAtoms, box, timeStep);
+	}
+
+}
 
 int main() {
 	
@@ -216,6 +430,6 @@ int main() {
 	cout << "Enter the temperature: ";
 	cin >> temp;
 
-	simulation(numberOfAtoms, temp);
+	simulation2(numberOfAtoms, temp);
 	return 0;
 }
